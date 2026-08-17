ccf5d371702e280c6baef2983d2dc219d0fdfba7
elifyuksek <yuksekelif02@gmail.com>
2026-08-18 02:03:55 +0300

    fix: add vercel rewrite config for SPA routing

--- backend/src/main/java/com/elifshop/ecommerce/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elifshop.ecommerce.repository;
-
-import com.elifshop.ecommerce.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- backend/src/main/java/com/elifshop/ecommerce/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elifshop.ecommerce.repository;
-
-import com.elifshop.ecommerce.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
